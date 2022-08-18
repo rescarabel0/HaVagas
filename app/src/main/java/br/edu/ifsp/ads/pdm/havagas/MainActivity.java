725ce1156a6e6270f9d94330b83938e00b6e4bc4
rescarabelo <rafael@scenario.ind.br>
2022-08-18 15:37:16 -0300

    Layout

--- app/src/main/java/br/edu/ifsp/ads/pdm/havagas/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/ads/pdm/havagas/MainActivity.java
@@ -1,14 +1,19 @@
 package br.edu.ifsp.ads.pdm.havagas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import br.edu.ifsp.ads.pdm.havagas.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        var activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.mainSv);
+
     }
 }
